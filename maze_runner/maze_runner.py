3a501d60c713b3f8c8277ed6b9de538099b4a66e
DustInABottle <dustin.laudahn@gmx.de>
2018-04-02 22:43:28 +0200

    feature(mazerunner):add layout to gui

--- maze_runner/maze_runner.py
+++ maze_runner/maze_runner.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import time
+from PIL import Image, ImageTk
 
 class Mazerunner(tk.Tk):
     '''
@@ -36,7 +38,44 @@ class MainPage(tk.Frame):
     '''
 
     def __init__(self, parent):
+        '''
+
+        :param parent:
+        '''
         super().__init__(parent)
+        #static elements
+        tk.Label(self, text='Current status:').grid(row=0, column=0, sticky='nsew')
+        tk.Label(self, text='Distance:').grid(row=1, column=0)
+        tk.Button(self, text='Start training.', command=self._start_training).grid(row=3)
+        tk.Label(self, text='Laps:').grid(row=2, column=0)
+
+        #dynamic elements
+        self.traveled_distance = tk.Label(self, text=0)
+        self.traveled_distance.grid(row=1, column=1)
+        first_image = Image.new('RGB', (100,100), color='black')
+        first_image= ImageTk.PhotoImage(first_image)
+
+        self.current_status=tk.Label(self, image=first_image)
+        self.current_status.image = first_image
+        self.current_status.grid(row=0, column=1)
+
+        self.laps = tk.Label(self, text=0)
+        self.laps.grid(row=2, column=1)
+
+        #resize configurations
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+
+    def _start_training(self):
+        ''''
+        Testwise content.
+        '''
 
 
 if __name__ == '__main__':
