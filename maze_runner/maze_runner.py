709b257a89391ed56635fc6279f6f4a11b40be9c
DustInABottle <dustin.laudahn@gmx.de>
2018-04-04 20:11:25 +0200

    feautre(gui):add label to display number of generation and global fitness

--- maze_runner/maze_runner.py
+++ maze_runner/maze_runner.py
@@ -45,9 +45,12 @@ class MainPage(tk.Frame):
         super().__init__(parent)
         #static elements
         tk.Label(self, text='Current status:').grid(row=0, column=0, sticky='nsew')
-        tk.Label(self, text='Distance:').grid(row=1, column=0)
-        tk.Button(self, text='Start training.', command=train_func).grid(row=3)
-        tk.Label(self, text='Laps:').grid(row=2, column=0)
+        tk.Label(self, text='Fitness of current model:').grid(row=1, column=0)
+        tk.Button(self, text='Start training.', command=train_func).grid(row=6)
+        tk.Label(self, text='Laps of current model:').grid(row=2, column=0)
+        tk.Label(self, text='------------------------------').grid(row=3)
+        tk.Label(self, text='Generation no.').grid(row=4, column=0)
+        tk.Label(self, text='Best global fitness:').grid(row=5, column=0)
 
         #dynamic elements
         self.traveled_distance = tk.Label(self, text=0)
@@ -62,12 +65,18 @@ class MainPage(tk.Frame):
         self.laps = tk.Label(self, text=0)
         self.laps.grid(row=2, column=1)
 
+        self.generation_no = tk.Label(self, text=0)
+        self.generation_no.grid(row=4, column=1)
+
+        self.global_fitness = tk.Label(self, text=0)
+        self.global_fitness.grid(row=5, column=1)
+
         #resize configurations for each row and column
-        for row_or_col in range(0,4):
+        for row_or_col in range(0,7):
             self.grid_rowconfigure(row_or_col, weight=1)
             self.grid_columnconfigure(row_or_col, weight=1)
 
-    def update_state(self, image, distance, laps):
+    def update_state(self, image, distance, laps, global_fitness, generation_no):
         '''
         Shows the given image in the image label of the GUI, updates distance and laps.
         :param image: The image to display
@@ -79,4 +88,6 @@ class MainPage(tk.Frame):
 
         self.traveled_distance['text'] = distance
         self.laps['text'] = laps
+        self.global_fitness['text'] = global_fitness
+        self.generation_no['text'] = generation_no
         self.update()
